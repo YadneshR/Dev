90bfade4e452f943fa7620e37dbd6d24c79276fa
YadneshR <yadneshmraut@gmail.com>
2025-09-04 18:40:42 +0000

    Initial commit 05/09/25

---
+++ archive_project.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#$Revision:001$
+
+#Variables:
+
+BASE=/home/yadneshLinux/Downloads
+DAYS=10
+DEPTH=1
+RUN=0
+
+#check if the directory is present or not 
+
+if [ ! -d $BASE  ]
+then 
+	echo "directory does not exist: $BASE "
+	exit 1
+fi
+
+#Create 'archive' folder if not present
+
+if [ ! -d $BASE/archive ]
+then 
+	mkdir $BASE/archive
+fi
+
+#FInd the list of files larger than 20 mb
+
+for i in $(find $BASE -maxdepth $DEPTH -type f -size +20M)
+do
+	if [ $RUN -eq 0  ]
+	then 
+		echo "[$(date "+%Y-%m-%d %H:%M:%S")] archiving $i ==> $BASE/archive"
+		gzip "$i" || exit 1
+		mv "$i.gz" "$BASE/archive" ||exit 1
+	fi
+
+done
+
+
+
